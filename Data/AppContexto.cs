f6b32cf9cd7d5e54a39f22503c019b1cae794bfb
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-18 13:25:40 -0400

    Add classe de Contexto

---
+++ Data/AppContexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TDSTecnologia.FaceAlbum.Web.Data
+{
+    public class AppContexto : DbContext
+    {
+        public AppContexto(DbContextOptions<AppContexto> opcoes) : base(opcoes)
+        {
+        }
+    }
+}
