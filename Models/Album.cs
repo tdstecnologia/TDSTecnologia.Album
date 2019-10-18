1f11e39ca67192f44773f449850420822326c493
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-18 13:29:29 -0400

    Add mapeamento classe Album

--- Models/Album.cs
+++ Models/Album.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace TDSTecnologia.FaceAlbum.Web.Models
 {
+    [Table("tb01_album")]
     public class Album
     {
+        [Key]
+        [Column("id")]
         public int AlbumId { get; set; }
+        [Column("titulo")]
         public string Titulo { get; set; }
+        [Column("descricao")]
         public string Descricao { get; set; }
+        [Column("capa")]
         public string Capa { get; set; }
+        [Column("dt_inicio")]
         public DateTime DataInicio { get; set; }
     }
 }
