de9f3154ee3728a8258484a6a5f52ca5ac5368d6
feliperyanribeirooliveira <felipe.ryan.tec@gmail.com>
2019-10-18 05:41:57 -0400

    Add classe Album

---
+++ Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TDSTecnologia.FaceAlbum.Web.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Capa { get; set; }
+        public DateTime DataInicio { get; set; }
+    }
+}
