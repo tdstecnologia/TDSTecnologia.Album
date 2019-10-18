6f198100b4865eb65385a7aab0f053e856571abc
Dherkyan Ribeiro <dherkyan@gmail.com>
2019-10-18 16:59:41 -0400

    Add metodo alterar - GET e POST

--- Controllers/AlbumController.cs
+++ Controllers/AlbumController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using TDSTecnologia.FaceAlbum.Web.Data;
 using TDSTecnologia.FaceAlbum.Web.Models;
@@ -36,6 +37,61 @@ namespace TDSTecnologia.FaceAlbum.Web.Controllers
             }
             return View(album);
         }
+
+
+        [HttpGet]
+        public IActionResult Alterar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var album = _context.Albuns.Find(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return View(album);
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(int id, Album album)
+        {
+            if (id != album.AlbumId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(album);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AlbumExists(album.AlbumId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(album);
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _context.Albuns.Any(e => e.AlbumId == id);
+        }
+
 
     }
 }
